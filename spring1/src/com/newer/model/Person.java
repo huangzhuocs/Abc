8db89d62f3467bfa696374044279e7bf3920e2f0
huangzhuocs <309963913@qq.com>
2017-06-01 14:27:22 +0800

    测试上传

---
+++ spring1/src/com/newer/model/Person.java
@@ -0,0 +1,13 @@
+package com.newer.model;
+
+import java.io.Serializable;
+
+public class Person implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private Integer age;
+	
+
+}
